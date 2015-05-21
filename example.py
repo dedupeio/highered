c1eb6e78534d3e32b1cbc1b8166864b2817c4253
Forest Gregg <fgregg@uchicago.edu>
2015-05-21 11:36:35 -0500

    parameters learned from real biz names

--- example.py
+++ example.py
@@ -3,53 +3,59 @@ import highered
 ed = highered.CRFEditDistance()
 
 print(ed('foo', 'bar'))
+print(ed('bar', 'foo'))
 
-X = [(u'caring hands a step ahead', u'el valor little tykes ii'),
- (u'dulles', u"chicago public schools o'keeffe, isabell c."),
- (u'erie neighborhood house fcch-carmen l. vega site',
-  u'erie neighborhood house fcch-servia galva site'),
- (u'chicago public schools dvorak math & science tech academy, anton',
-  u'chicago public schools perez, manuel'),
- (u'v & j day care center', u"henry booth house granny's day care center"),
- (u'home of life community dev. corp. - home of life just for you',
-  u'urban family and community centers'),
- (u'carole robertson center for learning fcch-ileana gonzalez',
-  u'carole robertson center for learning fcch-rhonda culverson'),
- (u'bethel new life bethel child development',
-  u'mary crane league mary crane center (lake & pulaski)'),
- (u'easter seals society of metropolitan chicago - stepping stones early/childhood lear',
-  u"marcy newberry association kenyatta's day care"),
- (u'westside holistic family services westside holistic family services',
-  u'childserv lawndale'),
- (u'higgins', u'higgins'),
- (u'ymca south side', u'ymca of metropolitan chicago - south side ymca'),
- (u'chicago commons association paulo freire',
-  u'chicago commons association paulo freire'),
- (u'fresh start daycare, inc.',
-  u'easter seals society of metropolitan chicago fresh start day care center'),
- (u'el valor teddy bear 3', u'teddy bear 3'),
- (u'chicago child care society chicago child care society',
-  u'chicago child care society-child and family dev center'),
- (u'hull house - uptown', u'uptown family care center')]
-Y = [u'distinct',
- u'distinct',
- u'distinct',
- u'distinct',
- u'distinct',
- u'distinct',
- u'distinct',
- u'distinct',
- u'distinct',
- u'distinct',
- u'match',
- u'match',
- u'match',
- u'match',
- u'match',
- u'match',
- u'match']
+print(ed('foo1', 'bar'))
+print(ed('bar', 'foo'1))
 
-ed.train(X, Y)
-print(ed.model.parameters)
-print(ed('foo', 'bar'))
+# X = [(u'caring hands a step ahead', u'el valor little tykes ii'),
+#  (u'dulles', u"chicago public schools o'keeffe, isabell c."),
+#  (u'erie neighborhood house fcch-carmen l. vega site',
+#   u'erie neighborhood house fcch-servia galva site'),
+#  (u'chicago public schools dvorak math & science tech academy, anton',
+#   u'chicago public schools perez, manuel'),
+#  (u'v & j day care center', u"henry booth house granny's day care center"),
+#  (u'home of life community dev. corp. - home of life just for you',
+#   u'urban family and community centers'),
+#  (u'carole robertson center for learning fcch-ileana gonzalez',
+#   u'carole robertson center for learning fcch-rhonda culverson'),
+#  (u'bethel new life bethel child development',
+#   u'mary crane league mary crane center (lake & pulaski)'),
+#  (u'easter seals society of metropolitan chicago - stepping stones early/childhood lear',
+#   u"marcy newberry association kenyatta's day care"),
+#  (u'westside holistic family services westside holistic family services',
+#   u'childserv lawndale'),
+#  (u'higgins', u'higgins'),
+#  (u'ymca south side', u'ymca of metropolitan chicago - south side ymca'),
+#  (u'chicago commons association paulo freire',
+#   u'chicago commons association paulo freire'),
+#  (u'fresh start daycare, inc.',
+#   u'easter seals society of metropolitan chicago fresh start day care center'),
+#  (u'el valor teddy bear 3', u'teddy bear 3'),
+#  (u'chicago child care society chicago child care society',
+#   u'chicago child care society-child and family dev center'),
+#  (u'hull house - uptown', u'uptown family care center')]
+# Y = [u'distinct',
+#  u'distinct',
+#  u'distinct',
+#  u'distinct',
+#  u'distinct',
+#  u'distinct',
+#  u'distinct',
+#  u'distinct',
+#  u'distinct',
+#  u'distinct',
+#  u'match',
+#  u'match',
+#  u'match',
+#  u'match',
+#  u'match',
+#  u'match',
+#  u'match']
 
+# ed.train(X, Y)
+# print(ed.model.parameters)
+# print(ed('foo', 'bar'))
+
+# import pdb
+# pdb.set_trace()
--- highered/__init__.py
+++ highered/__init__.py
@@ -1,6 +1,5 @@
 import pyhacrf
-from pyhacrf.pyhacrf import Hacrf
-from pyhacrf.features import StringPairFeatureExtractor
+from pyhacrf.pyhacrf import Hacrf, StringPairFeatureExtractor
 
 import numpy
 
@@ -8,14 +7,15 @@ import numpy
 class CRFEditDistance(object) :
     def __init__(self) :
         self.model = Hacrf(l2_regularization=1.0)
-        self.model.parameters = numpy.array([[-0.08833188,  0.  ],
-                                             [ 0.16700528,  0.  ],
-                                             [-0.11705577,  0.  ],
-                                             [ 0.14053809,  0.  ],
-                                             [-0.20092105,  0.  ],
-                                             [ 0.2766846 ,  0.  ],
-                                             [ 0.19652368,  0.  ],
-                                             [-0.21709615,  0.  ]])
+        self.model.parameters = numpy.array(
+            [[-0.06307111, -0.35686125, -0.24368323],
+             [ 0.05599825,  0.02427958,  0.24368323],
+             [-0.08365461, -0.01643165, -0.02468913],
+             [ 0.04300183, -0.03985499,  0.03919838],
+             [ 0.08278298,  0.16122849, -0.06341355],
+             [-0.04207757, -0.27139228,  0.04556619],
+             [ 0.17641326, -0.25142443, -0.15558055],
+             [-0.13576664,  0.08529407,  0.15891866]])
         self.model.classes = ['match', 'non-match']
 
         (self.model._state_machine, 
@@ -32,5 +32,7 @@ class CRFEditDistance(object) :
         self.model.fit(extracted_examples, labels, verbosity=1)
 
     def __call__(self, string_1, string_2) :
+        if not string_1 or not string_2 :
+            return numpy.nan
         features = self.feature_extractor.fit_transform(((string_1, string_2),))
         return self.model.predict_proba(features)[0,1]
