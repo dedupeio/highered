91889f75e6ec409845d14d4369f4b54248f95268
Forest Gregg <fgregg@uchicago.edu>
2016-06-21 14:20:17 -0500

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError :
 setup(
     name='highered',
     url='https://github.com/datamade/highered',
-    version='0.2.0',
+    version='0.2.1',
     author='Forest Gregg',
     author_email='fgregg@gmail.com',
     description='Learnable Edit Distance Using PyHacrf',
